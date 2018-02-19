f56e75f6bc9b1fa7a6d4539c32aad253951407e0
carol-solivos <carol.solivos@gmail.com>
2018-02-19 02:22:33 -0400

    obteniendo resultado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,27 @@
 //https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean
 //https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple
-
+const container = document.getElementById('container');
+init()
 const btnBoolean = document.getElementById('boolean');// boton seleccion verdadero o falso
 const btnmultiple = document.getElementById('multiple');// boton seleccion multiple
-const container = document.getElementById('container');
+
 btnBoolean.addEventListener('click', function () {
 	answerBoolean = btnBoolean.value;
 	getQuestions(answerBoolean);
 })
+
 btnmultiple.addEventListener('click', function () {
 	answermultiple = btnmultiple.value;
 	getQuestions(answermultiple);
 })
+
+function init() {
+	container.innerHTML = `<h2>Prueba tus conocimientos</h2>
+    <h4>¿Que estilo quieres jugar?</h4>
+    <button class="btn" id="multiple" value="multiple">Selección múltiple</button>
+    <button id="boolean" class="btn" value="boolean">Verdadero o falso</button>`;
+}
+
 function getQuestions(answer){
 	//creando objeto con el new
 	const qRequest = new XMLHttpRequest();
@@ -24,7 +34,6 @@ function getQuestions(answer){
 }
 
 function showQuestions(){
-	container.innerHTML = '';
 	// convertir la respuesta que esta en json en una respuesta javascript
 	const data = JSON.parse(this.responseText);
 	if (data.results[0].type === 'boolean') {
@@ -37,10 +46,29 @@ function showQuestions(){
 }
 
 function booleanQuestions(data) {
-	const responseQuestion = data.results[i].question;
-	const responseAnswer = data.results[i].correct_answer;
-	container.innerHTML = `<h1 class="">${responseQuestion}</h1><button class="btn" value="true">true</button><button class="btn" value="false">false</button>`;
+	let points = 0;
+	for (let i in data.results){
+		const responseQuestion = data.results[i].question;
+		const responseAnswer = data.results[i].correct_answer;
+		const responseIncorrectAnswer = data.results[i].incorrect_answers;
+		container.innerHTML = '';
+		container.innerHTML = `<h1 class="">${responseQuestion}</h1><button id="true" class="btn" value="True">true</button><button id="false" class="btn" value="False">false</button>`;
+		const btnTrue = document.getElementById('true');
+		const btnFalse = document.getElementById('false');
+		btnTrue.addEventListener('click', function () {
+			if (responseAnswer === btnTrue.val) {
+				points = points + 10;
+			}
+			if (responseAnswer !== btnTrue.val) {
+				points = points;
+			}
+		})	
+		
+	} return console.log(points)
+	
 }
+	// container.innerHTML = `<h1 class="">Acertaste un ${points}%</h1><button class="btn" value="True">true</button>`;
+
 
 function multipleQuestions(data) {
 	const responseQuestion = data.results[i].question;
